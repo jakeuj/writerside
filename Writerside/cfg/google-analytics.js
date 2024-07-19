ec6642f640552845d6800599e9e0414c72a0f3b6
jakeuj <jakeuj@hotmail.com>
2024-07-19 14:22:12 +0800

    Updated DOCKER_VERSION to '241.18775' and added Google AdSense script in google-analytics.js

--- Writerside/cfg/google-analytics.js
+++ Writerside/cfg/google-analytics.js
@@ -1,4 +1,5 @@
 <!-- Google tag (gtag.js) -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9431708219669190" crossorigin="anonymous"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-JTF8CLQ45J"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
